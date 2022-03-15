dd7964ae48b65bc61bd1a741c5be105f5dd87f2a
Adalia da Silva <ada.future.work@gmail.com>
2022-03-15 12:29:23 +0100

    finished 2. iteration

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+
+const movies = require('./movies.routes')
+router.use("./movies.routes", movies);
+
+const celebrity = require("./celebrities.routes");
+router.use("./celebrities.routes", celebrity);
+
+
 module.exports = router;
